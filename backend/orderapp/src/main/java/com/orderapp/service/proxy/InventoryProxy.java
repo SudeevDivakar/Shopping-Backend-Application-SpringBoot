a8f0beb40cf94ad031acd265b750bbaf447b9f8e
Sudeev <sudeev.divakar@gmail.com>
2025-03-09 22:24:17 +0530

    Enabled Circuit Breaker with Resilience4J

--- backend/orderapp/src/main/java/com/orderapp/service/proxy/InventoryProxy.java
+++ backend/orderapp/src/main/java/com/orderapp/service/proxy/InventoryProxy.java
@@ -1,17 +1,23 @@
 package com.orderapp.service.proxy;
 
 import com.orderapp.dto.InventoryDto;
-import com.orderapp.dto.ProductDto;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.*;
 
 @FeignClient(name = "inventoryapp", url = "localhost:8082")
 public interface InventoryProxy {
     @GetMapping("api/v1/inventory/{id}")
+    @CircuitBreaker(name = "inventory", fallbackMethod = "fallbackMethod")
     public InventoryDto retrieveInventoryStock(@PathVariable String id);
 
     @PutMapping("api/v1/inventory/{id}/{quantity}")
+    @CircuitBreaker(name = "inventory", fallbackMethod = "fallbackMethod")
     public InventoryDto updateStock(@PathVariable String id, @PathVariable Integer quantity, @RequestParam(name = "inc") Integer inc);
+
+    default InventoryDto fallbackMethod(String id, Throwable ex) {
+        throw new RuntimeException("Inventory Service TemporarilyDown");
+    }
 }
 
 
--- backend/orderapp/src/main/java/com/orderapp/service/proxy/ProductProxy.java
+++ backend/orderapp/src/main/java/com/orderapp/service/proxy/ProductProxy.java
@@ -1,6 +1,7 @@
 package com.orderapp.service.proxy;
 
 import com.orderapp.dto.ProductDto;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -8,5 +9,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 @FeignClient(name = "productapp", url = "localhost:8080")
 public interface ProductProxy {
     @GetMapping("api/v1/products/{id}")
+    @CircuitBreaker(name = "products", fallbackMethod = "fallbackMethod")
     public ProductDto retrieveProductDetails(@PathVariable String id);
+
+    default ProductDto fallbackMethod(String id, Throwable ex) {
+        throw new RuntimeException("Product Service Down");
+    }
 }
